5307b2de79b42e67217a6e2ddac87d5435144ec0
BaronClaps <126834072+BaronClaps@users.noreply.github.com>
2025-01-01 15:38:13 -0600

    Update FConstants.java and LConstants.java

--- TeamCode/src/main/java/indubitables/config/pedro/constants/FConstants.java
+++ TeamCode/src/main/java/indubitables/config/pedro/constants/FConstants.java
@@ -2,51 +2,92 @@ package indubitables.config.pedro.constants;
 
 import com.pedropathing.localization.Localizers;
 import com.pedropathing.follower.FollowerConstants;
+import com.pedropathing.pathgen.Vector;
 import com.pedropathing.util.CustomFilteredPIDFCoefficients;
 import com.pedropathing.util.CustomPIDFCoefficients;
+import com.pedropathing.util.KalmanFilterParameters;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 public class FConstants {
     static {
-        FollowerConstants.localizers = Localizers.THREE_WHEEL;
-
+        FollowerConstants.localizers = Localizers.PINPOINT;
+        
         FollowerConstants.leftFrontMotorName = "leftFront";
         FollowerConstants.leftRearMotorName = "leftRear";
         FollowerConstants.rightFrontMotorName = "rightFront";
         FollowerConstants.rightRearMotorName = "rightRear";
+        
+        FollowerConstants.xMovement = 74.4652;
+        FollowerConstants.yMovement = 61.5559;
 
-        FollowerConstants.leftFrontMotorDirection = DcMotorSimple.Direction.REVERSE;
-        FollowerConstants.leftRearMotorDirection = DcMotorSimple.Direction.REVERSE;
-        FollowerConstants.rightFrontMotorDirection = DcMotorSimple.Direction.FORWARD;
-        FollowerConstants.rightRearMotorDirection = DcMotorSimple.Direction.FORWARD;
+        FollowerConstants.forwardZeroPowerAcceleration = -30.4793;
+        FollowerConstants.lateralZeroPowerAcceleration = -63.8818;
+        
+        FollowerConstants.mass = 12.4;
+        
+        FollowerConstants.centripetalScaling = 0.0003;
 
-        FollowerConstants.mass = 13;
+        FollowerConstants.zeroPowerAccelerationMultiplier = 7;
 
-        FollowerConstants.xMovement = 57.8741;
-        FollowerConstants.yMovement = 52.295;
-
-        FollowerConstants.forwardZeroPowerAcceleration = -41.278;
-        FollowerConstants.lateralZeroPowerAcceleration = -59.7819;
-
-        FollowerConstants.translationalPIDFCoefficients = new CustomPIDFCoefficients(0.1,0,0.01,0);
-        FollowerConstants.useSecondaryTranslationalPID = false;
-        FollowerConstants.secondaryTranslationalPIDFCoefficients = new CustomPIDFCoefficients(0.1,0,0.01,0); // Not being used, @see useSecondaryTranslationalPID
-
-        FollowerConstants.headingPIDFCoefficients = new CustomPIDFCoefficients(2,0,0.1,0);
-        FollowerConstants.useSecondaryHeadingPID = false;
-        FollowerConstants.secondaryHeadingPIDFCoefficients = new CustomPIDFCoefficients(2,0,0.1,0); // Not being used, @see useSecondaryHeadingPID
-
-        FollowerConstants.drivePIDFCoefficients = new CustomFilteredPIDFCoefficients(0.1,0,0,0.6,0);
-        FollowerConstants.useSecondaryDrivePID = false;
-        FollowerConstants.secondaryDrivePIDFCoefficients = new CustomFilteredPIDFCoefficients(0.1,0,0,0.6,0); // Not being used, @see useSecondaryDrivePID
-
-        FollowerConstants.zeroPowerAccelerationMultiplier = 4;
-        FollowerConstants.centripetalScaling = 0.0005;
-
-        FollowerConstants.pathEndTimeoutConstraint = 500;
-        FollowerConstants.pathEndTValueConstraint = 0.995;
         FollowerConstants.pathEndVelocityConstraint = 0.1;
         FollowerConstants.pathEndTranslationalConstraint = 0.1;
         FollowerConstants.pathEndHeadingConstraint = 0.007;
+        FollowerConstants.pathEndTValueConstraint = 0.95;
+        FollowerConstants.pathEndTimeoutConstraint = 50;
+
+        FollowerConstants.useSecondaryTranslationalPID = true;
+        FollowerConstants.useSecondaryHeadingPID = true;
+        FollowerConstants.useSecondaryDrivePID = true;
+
+        FollowerConstants.translationalPIDFCoefficients = new CustomPIDFCoefficients(
+                0.05,
+                0,
+                0.0001,
+                0);
+        FollowerConstants.translationalIntegral = new CustomPIDFCoefficients(
+                0,
+                0,
+                0,
+                0);
+        FollowerConstants.translationalPIDFFeedForward = 0.015;
+        FollowerConstants.secondaryTranslationalPIDFCoefficients = new CustomPIDFCoefficients(
+                0.1,
+                0,
+                0.05,
+                0);
+        FollowerConstants.secondaryTranslationalIntegral = new CustomPIDFCoefficients(
+                0,
+                0,
+                0,
+                0);
+        FollowerConstants.secondaryTranslationalPIDFFeedForward = 0.0005;
+
+        FollowerConstants.headingPIDFCoefficients = new CustomPIDFCoefficients(
+                5,
+                0,
+                0.5,
+                0);
+        FollowerConstants.headingPIDFFeedForward = 0.01;
+        FollowerConstants.secondaryHeadingPIDFCoefficients = new CustomPIDFCoefficients(
+                3,
+                0,
+                0.1,
+                0);
+        FollowerConstants.secondaryHeadingPIDFFeedForward = 0.0005;
+
+        FollowerConstants.drivePIDFCoefficients = new CustomFilteredPIDFCoefficients(
+                0.01,
+                0,
+                0.0001,
+                0.6,
+                0);
+        FollowerConstants.drivePIDFFeedForward = 0.01;
+        FollowerConstants.secondaryDrivePIDFCoefficients = new CustomFilteredPIDFCoefficients(
+                0.02,
+                0,
+                0.0005,
+                0.6,
+                0);
+        FollowerConstants.secondaryDrivePIDFFeedForward = 0.01;
     }
 }
--- TeamCode/src/main/java/indubitables/config/pedro/constants/LConstants.java
+++ TeamCode/src/main/java/indubitables/config/pedro/constants/LConstants.java
@@ -5,18 +5,21 @@ import com.pedropathing.localization.constants.*;
 
 public class LConstants {
     static {
-        ThreeWheelConstants.forwardTicksToInches = .001989436789;
-        ThreeWheelConstants.strafeTicksToInches = .001989436789;
-        ThreeWheelConstants.turnTicksToInches = .001989436789;
-        ThreeWheelConstants.leftY = 1;
-        ThreeWheelConstants.rightY = -1;
-        ThreeWheelConstants.strafeX = -2.5;
-        ThreeWheelConstants.leftEncoder_HardwareMapName = "leftFront";
-        ThreeWheelConstants.rightEncoder_HardwareMapName = "rightRear";
-        ThreeWheelConstants.strafeEncoder_HardwareMapName = "rightFront";
-        ThreeWheelConstants.leftEncoderDirection = Encoder.REVERSE;
-        ThreeWheelConstants.rightEncoderDirection = Encoder.REVERSE;
-        ThreeWheelConstants.strafeEncoderDirection = Encoder.FORWARD;
+
+        PinpointConstants.forwardY = 1.25;
+        PinpointConstants.strafeX = 1.25;
+        PinpointConstants.hardwareMapName = "pinpoint";
+
+        PinpointConstants.useYawScalar = false;
+        PinpointConstants.yawScalar = 1.0;
+
+        PinpointConstants.useCustomEncoderResolution = false;
+        PinpointConstants.encoderResolution = GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD;
+        PinpointConstants.customEncoderResolution = 13.26291192;
+
+        PinpointConstants.forwardEncoderDirection = GoBildaPinpointDriver.EncoderDirection.REVERSED;
+        PinpointConstants.strafeEncoderDirection = GoBildaPinpointDriver.EncoderDirection.FORWARD;
+
     }
 }
 
